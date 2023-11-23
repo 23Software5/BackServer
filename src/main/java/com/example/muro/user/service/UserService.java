6888f060d2e73d4fc73397bcd35e7e75922884b1
Sowon <cherishwish12@gmail.com>
2023-11-24 03:02:47 +0900

    회원도메인 getter 수정, service 추가

--- src/main/java/com/example/muro/user/service/UserService.java
+++ src/main/java/com/example/muro/user/service/UserService.java
@@ -1,16 +1,42 @@
 package com.example.muro.user.service;
 
-import com.example.muro.user.dto.UserSignUpDto;
+import com.example.muro.user.domain.User;
 import com.example.muro.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Transactional
 @Service
 public class UserService {
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
+    //회원가입
+
+    public Long join(User user){
+        this.validateDuplicateUser(user);
+        this.userRepository.save(user);
+        return user.getId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        //EXCEPTION
+        List<User> findUsers = userRepository.findByNickname(user.getNickname());
+        if (!findUsers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<User> findUsers(){
+        return userRepository.findAll();
+    }
+    public User findOne(Long userId){
+        return userRepository.findOne(userId);
+    }
+
+    //회원 전체 조회
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
