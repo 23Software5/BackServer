7ce7028024244e58f02ef3af93d2c8b711854a09
Sowon <cherishwish12@gmail.com>
2023-11-28 23:49:13 +0900

    sql구문 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (user_id, email, nickname, password) VALUES (1, 'example@example.com', 'user123', 'hashed_password');
-INSERT INTO funeralhall (fh_id ,fh_intro ,fh_link ,fh_location ,fh_name ,fh_telephone ) VALUES (2,'hihi', 'https://imyourwish.tistory.com/', '서울시 송파구 위례순환로','소원이네 집','01051597233');
+INSERT INTO funeralhall (fh_id ,fh_intro ,fh_link ,fh_location ,fh_name ,fh_telephone ) VALUES (2,'hihi', 'https://lms.duksung.ac.kr/login.php', 'home','house','01051597233');
